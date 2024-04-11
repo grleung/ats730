c19c76e726dbb64a13dea19d40451a8e60f86a10
grleung <gabrielle.leung@colostate.edu>
2024-04-11 16:44:41 -0600

    added limiter to water vapor

--- mac_model/src/microphysics.f90
+++ mac_model/src/microphysics.f90
@@ -39,8 +39,8 @@ module microphysics
 
     subroutine add_tend_micro
         use model_vars, only: rvp_tend_total, rcp_tend_total, rrp_tend_total, thp_tend_total,pib &
-                            , rain2vap,cld2rain_accr,cld2rain_auto,vap2cld, rcp, rrp
-        use run_constants, only: nx,ny,nz,rd2t
+                            , rain2vap,cld2rain_accr,cld2rain_auto,vap2cld, rcp, rrp,rvp,rvb
+        use run_constants, only: nx,ny,nz,rd2t,rdt,dt
         use constants, only: cp, lv
 
         implicit none
@@ -49,7 +49,7 @@ module microphysics
         integer :: iy ! counter for y-coordinate
         integer :: ix ! counter for x-coordinate
 
-        real :: cldavail, cldsink, cldrat, rainavail
+        real :: cldavail, cldsink, cldrat, rainavail, vapavail
 
         do iz = 1, nz
             do iy=1,ny
@@ -68,7 +68,7 @@ module microphysics
                         cld2rain_auto(ix,iy,iz) = cld2rain_auto(ix,iy,iz) - ((1-cldrat) * (cldsink - cldavail))
                     endif
 
-                    rcp_tend_total(ix,iy,iz) = rcp_tend_total(ix,iy,iz) + (rd2t*vap2cld(ix,iy,iz)) - cld2rain_accr(ix,iy,iz) - cld2rain_auto(ix,iy,iz) 
+                    rcp_tend_total(ix,iy,iz) = rcp_tend_total(ix,iy,iz) + (rdt*vap2cld(ix,iy,iz)) - cld2rain_accr(ix,iy,iz) - cld2rain_auto(ix,iy,iz) 
 
 
                     rainavail = rrp(ix,iy,iz,1)*rd2t
@@ -78,8 +78,15 @@ module microphysics
                     endif
 
                     rrp_tend_total(ix,iy,iz) = rrp_tend_total(ix,iy,iz) + cld2rain_accr(ix,iy,iz) + cld2rain_auto(ix,iy,iz) - rain2vap(ix,iy,iz)
-                    thp_tend_total(ix,iy,iz) = thp_tend_total(ix,iy,iz) + ((lv/(cp*pib(iz))) * ((rd2t*vap2cld(ix,iy,iz)) - rain2vap(ix,iy,iz)))
-                    rvp_tend_total(ix,iy,iz) = rvp_tend_total(ix,iy,iz) - (rd2t*vap2cld(ix,iy,iz)) + rain2vap(ix,iy,iz)
+
+                    vapavail = (rvp(ix,iy,iz,1)+rvb(iz))*rd2t
+
+                    if (rdt*vap2cld(ix,iy,iz)>vapavail) then
+                        vap2cld(ix,iy,iz) = vapavail*dt
+                    endif
+
+                    thp_tend_total(ix,iy,iz) = thp_tend_total(ix,iy,iz) + ((lv/(cp*pib(iz))) * ((rdt*vap2cld(ix,iy,iz)) - rain2vap(ix,iy,iz)))
+                    rvp_tend_total(ix,iy,iz) = rvp_tend_total(ix,iy,iz) - (rdt*vap2cld(ix,iy,iz)) + rain2vap(ix,iy,iz)
                     
                 enddo
             enddo
