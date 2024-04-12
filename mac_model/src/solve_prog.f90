79739e994b722e862a66144239419083f0637be0
grleung <gabrielle.leung@colostate.edu>
2024-04-11 18:14:30 -0600

    fixed typo in w buoyancy term + moved saturation adjustment to last

--- mac_model/src/solve_prog.f90
+++ mac_model/src/solve_prog.f90
@@ -47,9 +47,10 @@ module solve_prog
         ! temporarily apply the tendencies calculated above
         call apply_tends
         print*,'apply tends'
-        
-        ! saturation adjustment    
-        call sat_adjust  
+
+        ! check water mixing ratios are non negative after applying all tendencies
+        call check_negs
+        print*,'check neg'
         
         ! then update rvp,rcp,rrp with microphysics conversions
         call add_tend_micro
@@ -59,6 +60,18 @@ module solve_prog
         call apply_tends
         print*,'apply tends'
 
+        ! check water mixing ratios are non negative after applying all tendencies
+        call check_negs
+        print*,'check neg'
+        
+        ! saturation adjustment    
+        call sat_adjust  
+        print*,'sat adjusted'
+
+        ! apply the tendencies calculated above
+        !call apply_tends
+        !print*,'apply tends'
+        
         ! check water mixing ratios are non negative after applying all tendencies
         call check_negs
         print*,'check neg'
@@ -308,7 +321,7 @@ module solve_prog
                     ! term in w-tendency equation: buoyancy term 
                     w_buoy(ix,iy,iz) = g *((thp(ix,iy,iz,2)+thp(ix,iy,iz-1,2))/(thb(iz)+thb(iz-1))      &   
                                               + (0.61 * 0.5 * (rvp(ix,iy,iz,2)+rvp(ix,iy,iz-1,2)))                                &
-                                              - (0.5 * (rcp(ix,iy,iz,2)+rcp(ix,iy,iz-1,2)) + (rrp(ix,iy,iz,2)+rrp(ix,iy,iz-1,2))))
+                                              - (0.5 * ((rcp(ix,iy,iz,2)+rcp(ix,iy,iz-1,2)) + (rrp(ix,iy,iz,2)+rrp(ix,iy,iz-1,2)))))
 
                     ! term in w-tendency equation: x diffusion
                     w_xdiff(ix,iy,iz) = kmx * rdx * rdx * (wp(ix-1,iy,iz,1) - (2*wp(ix,iy,iz,1)) + wp(ix+1,iy,iz,1))
